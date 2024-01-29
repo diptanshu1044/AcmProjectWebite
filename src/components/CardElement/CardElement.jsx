e64415858a42c6b023230c152cf0f9fe6fe1712e
diptanshu1044 <diptanshubanerjee123@gmail.com>
2024-01-30 00:02:39 +0530

    some changes

--- src/components/CardElement/CardElement.jsx
+++ src/components/CardElement/CardElement.jsx
@@ -10,14 +10,7 @@ import { CardHeader } from '@mui/material';
 
 const CardElement = ({ title, content, style }) => {
   return (
-    <Card sx={{ width: '330px', backgroundColor: '#6f41f6', padding: '70px 35px 46px 50px', margin: '2rem', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)' }}>
-      <h2 style={{ fontSize: '1.5rem', color: 'white', fontWeight: 'bold', marginBottom: '1rem', marginLeft: '10px', marginRight: '10px' }}>
-        {title}
-      </h2>
-      <CardContent sx={{ color: 'white' }}>
-        {content}
-      </CardContent>
-    </Card>
+    <></>
   )
 }
 
--- src/components/SixthComponent/SixthComponent.jsx
+++ src/components/SixthComponent/SixthComponent.jsx
@@ -1,4 +1,12 @@
 import CardElement from "../CardElement/CardElement";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { CardHeader } from '@mui/material';
 
 const SixthComponent = () => {
   let i = 1;
@@ -26,11 +34,31 @@ const SixthComponent = () => {
           Why You Should <br /> Choose Us
         </h2>
       </div>
-      <div className="flex flex-grow justify-evenly">
-        {cardInfo.map((item) => {
-          
-          return <CardElement title={item.title} content={item.content} key={i++} />
-        })}
+      <div className="flex justify-around">
+        <Card sx={{ width: '330px', backgroundColor: '#59c98d', padding: '70px 35px 46px 50px', margin: '', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)' }}>
+          <h2 style={{ fontSize: '1.5rem', color: 'white', fontWeight: 'bold', marginBottom: '1rem', marginLeft: '10px', marginRight: '10px' }}>
+            Complete Product For Your Business
+          </h2>
+          <CardContent sx={{ color: 'white' }}>
+            We build the template that is rich in content and has a good user interface to deliver a premium user experience for your customers.
+          </CardContent>
+        </Card>
+        <Card sx={{ width: '330px', backgroundColor: '#5ac9fa', padding: '70px 35px 46px 50px', margin: '', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)' }}>
+          <h2 style={{ fontSize: '1.5rem', color: 'white', fontWeight: 'bold', marginBottom: '1rem', marginLeft: '10px', marginRight: '10px' }}>
+            SEO and SMM Friendly
+          </h2>
+          <CardContent sx={{ color: 'white' }}>
+            Our template is SEO and SMM friendly that aligns with your digital marketing strategies to bring more organic traffic to your website.
+          </CardContent>
+        </Card>
+        <Card sx={{ width: '330px', backgroundColor: '#6f41f6', padding: '70px 35px 46px 50px', margin: '', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)' }}>
+          <h2 style={{ fontSize: '1.5rem', color: 'white', fontWeight: 'bold', marginBottom: '1rem', marginLeft: '10px', marginRight: '10px' }}>
+            Nicely Organized Code
+          </h2>
+          <CardContent sx={{ color: 'white' }}>
+            The coding structure of our items is organized so other developers can easily recognize the pattern and make updates as needed.
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
